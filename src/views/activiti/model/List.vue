1c1762d9716e94f4b67e8cbe4a367cb20b826e90
tengchong <26172583@qq.com>
2021-01-06 23:33:23 +0800

    添加流程模型编辑

--- src/views/activiti/model/List.vue
+++ src/views/activiti/model/List.vue
@@ -1,57 +1,86 @@
 <template>
-  <e-pro-table title="系统参数" :advanced.sync="advanced">
-    <template slot="query">
-      <a-col :xxl="6" :xl="8" :lg="12" :sm="24">
-        <a-form-model-item label="key">
-          <a-input v-model="queryParam.sysKey"/>
-        </a-form-model-item>
-      </a-col>
-      <a-col :xxl="6" :xl="8" :lg="12" :sm="24">
-        <a-form-model-item label="value">
-          <a-input v-model="queryParam.value"/>
-        </a-form-model-item>
-      </a-col>
-      <template v-if="advanced">
+  <div>
+    <e-pro-table title="系统参数" :no-advanced="true">
+      <template slot="query">
         <a-col :xxl="6" :xl="8" :lg="12" :sm="24">
-          <a-form-model-item label="类型">
-            <e-dict-select type="dataType" v-model="queryParam.type" @change="$refs.table.refresh(true)"/>
+          <a-form-model-item label="名称">
+            <a-input v-model="queryParam.name"/>
+          </a-form-model-item>
+        </a-col>
+        <a-col :xxl="6" :xl="8" :lg="12" :sm="24">
+          <a-form-model-item label="标识">
+            <a-input v-model="queryParam.key"/>
           </a-form-model-item>
         </a-col>
       </template>
-    </template>
 
-    <template slot="button">
-      <e-btn-add to="/sys/config/add"/>
-      <e-btn-remove-batch :ids="selectedRowKeys" :click-callback="remove"/>
-    </template>
+      <template slot="button">
+        <a-button type="primary" icon="plus" @click="handleAdd">新增</a-button>
+        <e-btn-remove-batch :ids="selectedRowKeys" :click-callback="remove"/>
+      </template>
 
-    <template slot="table">
-      <s-table
-        ref="table"
-        :columns="columns"
-        :data="loadData"
-        :alert="true"
-        :rowSelection="rowSelection"
-        showPagination="auto"
-      >
-        <span slot="type" slot-scope="text, record">
-          <e-dict-tag type="dataType" :code="record.type"/>
-        </span>
-
-        <span slot="action" slot-scope="text, record">
-          <template>
-            <e-btn-edit :to="`/sys/config/input`" :tab-name="record.key" :id="record.id"/>
-            <e-btn-remove :id="record.id" :divider="false" :click-callback="remove"/>
-          </template>
-        </span>
-      </s-table>
-    </template>
-  </e-pro-table>
+      <template slot="table">
+        <s-table
+          ref="table"
+          :columns="columns"
+          :data="loadData"
+          :alert="true"
+          :rowSelection="rowSelection"
+          showPagination="auto"
+        >
+          <span slot="version" slot-scope="text">
+            <a-tag color="blue">v.{{ text }}</a-tag>
+          </span>
+          <span slot="action" slot-scope="text, record">
+            <template>
+              <e-btn-edit :to="`/iframe?path=/activiti-editor/modeler.html?modelId=${record.id}`" :tab-name="record.name"/>
+              <a-tooltip placement="top">
+                <template slot="title">
+                  <span>部署</span>
+                </template>
+                <a-button type="primary" size="small" icon="check"/>
+              </a-tooltip>
+              <a-divider type="vertical" />
+              <a-tooltip placement="top">
+                <template slot="title">
+                  <span>导出</span>
+                </template>
+                <a-button size="small" icon="download"/>
+              </a-tooltip>
+              <a-divider type="vertical" />
+              <e-btn-remove :id="record.id" :divider="false" :click-callback="remove"/>
+            </template>
+          </span>
+        </s-table>
+      </template>
+    </e-pro-table>
+    <a-modal v-model="addModalVisible" title="新增模型" @ok="saveData" okText="保存并关闭">
+      <a-form-model
+        ref="form"
+        :model="model"
+        :rules="rules"
+        :label-col="formLayout.labelCol"
+        :wrapper-col="formLayout.wrapperCol">
+        <a-row class="form-row" :gutter="16">
+          <a-col>
+            <a-form-model-item label="名称" prop="name">
+              <a-input v-model="model.name"/>
+            </a-form-model-item>
+          </a-col>
+          <a-col>
+            <a-form-model-item label="标识" prop="key">
+              <a-input v-model="model.key"/>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+    </a-modal>
+  </div>
 </template>
 
 <script>
 import { STable, Ellipsis } from '@/components'
-import { select, remove } from '@/api/activiti/model'
+import { select, remove, save } from '@/api/activiti/model'
 import EDictTag from '@/components/Easy/data-entry/DictTag'
 import EDictSelect from '@/components/Easy/data-entry/DictSelect'
 import EBtnAdd from '@/components/Easy/general/BtnAdd'
@@ -60,6 +89,8 @@ import EBtnRemove from '@/components/Easy/general/BtnRemove'
 import EBtnRemoveBatch from '@/components/Easy/general/BtnRemoveBatch'
 import EProTable from '@/components/Easy/data-display/ProTable'
 import { formatDate } from '@/utils/util'
+import { FORM_LAYOUT } from '@/utils/const/form'
+import { saveSuccessTip } from '@/utils/tips'
 
 const columns = [
   {
@@ -79,7 +110,7 @@ const columns = [
     dataIndex: 'version',
     sorter: true,
     width: 100,
-    customRender: (text) => `v.${text}`
+    scopedSlots: { customRender: 'version' }
   },
   {
     title: '创建时间',
@@ -98,7 +129,7 @@ const columns = [
   {
     title: '操作',
     dataIndex: 'action',
-    width: 160,
+    width: 180,
     fixed: 'right',
     scopedSlots: { customRender: 'action' }
   }
@@ -120,12 +151,25 @@ export default {
   data () {
     this.columns = columns
     return {
-      // 高级搜索 展开/关闭
-      advanced: false,
       // 查询参数
       queryParam: {},
       selectedRowKeys: [],
-      selectedRows: []
+      selectedRows: [],
+
+      // 新增窗口
+      addModalVisible: false,
+      formLayout: FORM_LAYOUT,
+      model: {},
+      rules: {
+        name: [
+          { required: true, message: '请输入名称', trigger: 'blur' },
+          { max: 50, message: '名称字数不能超过50个字符', trigger: 'blur' }
+        ],
+        key: [
+          { required: true, message: '请输入标识', trigger: 'blur' },
+          { max: 50, message: '标识字数不能超过50个字符', trigger: 'blur' }
+        ]
+      }
     }
   },
   activated () {
@@ -155,6 +199,21 @@ export default {
     remove (id) {
       remove(id).then(res => {
         this.$refs.table.refresh(true)
+      })
+    },
+    handleAdd () {
+      this.addModalVisible = true
+      this.model = {}
+    },
+    saveData () {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          save(this.model).then((res) => {
+            this.$refs.table.refresh(true)
+            saveSuccessTip()
+            this.addModalVisible = false
+          })
+        }
       })
     }
   }
